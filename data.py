cac83ae923d009f85ad49aa8877752bdaaab7c53
Anand Shetler <anandshetler@gmail.com>
2020-04-07 11:09:13 -0400

    Use google sheets api

--- data.py
+++ data.py
@@ -1,14 +1,10 @@
-import requests
-import re
-from bs4 import BeautifulSoup
+from googleapiclient import discovery
+from google.oauth2 import service_account
+from pprint import pprint #keep pprint just for testing, can remove later
 
-def get_all_tables():
-    page = requests.get("https://www.kff.org/health-costs/issue-brief/state-data-and-policy-actions-to-address-coronavirus/")
-    soup = BeautifulSoup(page.content, features="html.parser")
-    tables = soup.find_all('div', attrs={'data-app-js':re.compile("[\{]")})
-    ret = []
-    for x in tables:
-        ret.append(x['data-app-js'])
-    return ret
-
-# print(get_all_tables())
+def get_sheet_data(spreadsheet_id, range_):
+    credentials = service_account.Credentials.from_service_account_file("sheetsAPICredentials.json")
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+    request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_)
+    response = request.execute()
+    return response
--- main.py
+++ main.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", data=data.get_all_tables())
+    return render_template("index.html", 
+                            cases_by_state=data.get_sheet_data('1BDbzCX0-m673QatijTXJhq7dh9p1RriQmUfBcUmbvZg', 'A1:E54'),
+                            policies_by_state=data.get_sheet_data('14EwUHhMnksYrYrNV7cX5rpCvASGQlcY3ZpKx-ErCmUE', 'A1:I54'))
