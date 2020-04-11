6e02fd9cbbdaf4397415f2da65b9d30d731c5fcc
Anand Shetler <anandshetler@gmail.com>
2020-04-11 15:52:33 -0400

    rename func

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import data
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template("index.html", 
                             cases_by_state=data.get_sheet_data('1BDbzCX0-m673QatijTXJhq7dh9p1RriQmUfBcUmbvZg', 'A1:E54'),
                             policies_by_state=data.get_sheet_data('14EwUHhMnksYrYrNV7cX5rpCvASGQlcY3ZpKx-ErCmUE', 'A1:I54'))
