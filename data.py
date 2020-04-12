a302e29600cb0fb012f53be38a042ece6ab5dc0a
Anand Shetler <anandshetler@gmail.com>
2020-04-12 18:05:47 -0400

    Safe map stuff works

--- data.py
+++ data.py
@@ -7,6 +7,8 @@ import re
 from bs4 import BeautifulSoup
 import json
 
+geo_id_dict = { "Alabama": 1,"Alaska": 2,"Arizona": 4,"Arkansas": 5,"California": 6,"Colorado": 8,"Connecticut": 9,"Delaware": 10,"District of Columbia":11,"Florida": 12,"Georgia": 13,"Hawaii": 15,"Idaho": 16,"Illinois": 17,"Indiana": 18,"Iowa": 19,"Kansas": 20,"Kentucky": 21,"Louisiana": 22,"Maine": 23,"Maryland": 24,"Massachusetts": 25,"Michigan": 26,"Minnesota": 27,"Mississippi": 28,"Missouri": 29,"Montana": 30,"Nebraska": 31,"Nevada": 32,"New Hampshire": 33,"New Jersey": 34,"New Mexico": 35,"New York": 36,"North Carolina": 37,"North Dakota": 38,"Ohio": 39,"Oklahoma": 40,"Oregon": 41,"Pennsylvania": 42,"Rhode Island": 44,"South Carolina": 45,"South Dakota": 46,"Tennessee": 47,"Texas": 48,"Utah": 49,"Vermont": 50,"Virginia": 51,"Washington": 53,"West Virginia": 54,"Wisconsin":55,"Wyoming": 56}
+
 def get_sheet_data(spreadsheet_id, range_):
     credentials = service_account.Credentials.from_service_account_file("sheetsAPICredentials.json")
     service = discovery.build('sheets', 'v4', credentials=credentials)
@@ -19,3 +21,15 @@ def get_state_policies_key():
     soup = BeautifulSoup(page.content, features="html.parser")
     tables = soup.find_all('div', attrs={'data-app-js':re.compile("[\{]")})
     return json.loads(tables[3]['data-app-js'])['gdocs_key']
+
+def gen_map_data(cases_table, col, policies_table=False):
+    ret = []
+    if policies_table:
+        for i in range(3,len(policies_table)):
+            ret.append([geo_id_dict[cases_table[i][0]],float(cases_table[i][2]) if not policies_table[i][col] == "-" else -1])
+    else:
+        for i in range(3,len(cases_table)):
+            ret.append([geo_id_dict[cases_table[i][0]],float(cases_table[i][col].replace(",",""))])
+    return ret
+
+# pprint(gen_map_data(get_sheet_data('1BDbzCX0-m673QatijTXJhq7dh9p1RriQmUfBcUmbvZg', 'A1:E54'),3))
--- main.py
+++ main.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html", 
-                            cases_by_state=data.get_sheet_data('1BDbzCX0-m673QatijTXJhq7dh9p1RriQmUfBcUmbvZg', 'A1:E54'),
-                            policies_by_state=data.get_sheet_data(data.get_state_policies_key(), 'A1:I54'))
+    cases_by_state_table = data.get_sheet_data('1BDbzCX0-m673QatijTXJhq7dh9p1RriQmUfBcUmbvZg', 'A1:E54')
+    policies_by_state_table = data.get_sheet_data(data.get_state_policies_key(), 'A1:I54')
+    return render_template(
+        "index.html", 
+        cases_by_state=cases_by_state_table,
+        policies_by_state=policies_by_state_table,
+        num_of_cases_map=data.gen_map_data(cases_by_state_table,1),
+        cases_per_mil_map=data.gen_map_data(cases_by_state_table,2),
+        map_data_test=data.gen_map_data(cases_by_state_table,3,policies_by_state_table))
